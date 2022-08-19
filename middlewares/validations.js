8dabfcd5ec1fde0439d3c3c229d5f9500f1a197c
Wellington <wellingtonmachioni@gmail.com>
2022-08-19 12:11:40 -0300

    Implementado validação para cadastro de produtos

---
+++ middlewares/validations.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+
+const schemaProduct = Joi.object({
+  name: Joi.string().min(5).required().messages({
+    'any.required': '"name" is required',
+    'string.empty': '"name" is required',
+    'string.min': '"name" length must be at least 5 characters long',
+  }),
+});
+
+const stausCode = (error) => {
+  const errorType = error.details[0].type;
+  if (errorType === 'string.empty' || errorType === 'any.required') {
+    return 400;
+  }
+  return 422;
+};
+
+const productValidade = (req, res, next) => { 
+  const values = req.body;
+
+  const { error } = schemaProduct.validate(values);
+  console.log(error.details[0].type);
+
+  if (error) return res.status(stausCode(error)).json({ message: error.details[0].message });
+
+  next();
+};
+
+module.exports = { productValidade };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const productController = require('../controllers/productController');
+const validate = require('../middlewares/validations');
 
 const productRoute = express.Router();
 
@@ -7,6 +8,6 @@ productRoute.get('/', productController.getAll);
 
 productRoute.get('/:id', productController.getById);
 
-productRoute.post('/', productController.create);
+productRoute.post('/', validate.productValidade, productController.create);
 
 module.exports = productRoute;
