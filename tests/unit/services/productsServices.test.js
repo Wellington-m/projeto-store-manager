bae52a12e284328c5343ebf51b6285c987c69c49
Wellington <wellingtonmachioni@gmail.com>
2022-08-20 18:25:07 -0300

    Implementado testes unitários na camada service da função getAll

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -1,54 +1,54 @@
 const { expect } = require('chai');
 const { describe } = require('mocha');
 const sinon = require('sinon');
+const productService = require('../../../services/productService');
 const productModel = require('../../../models/productModel');
-const connection = require('../../../models/connection');
 const { allProductsResponse } = require('../dataMock');
 
 describe('Busca todos os produtos no banco de dados', () => {
   describe('Não existe nenhum produdo cadastrado', () => {
     before(() => {
-      const executeResult = [[], []];
-      sinon.stub(connection, 'execute').resolves(executeResult);
+      sinon.stub(productModel, 'getAll').resolves([]);
     });
-    after(() => { 
-      connection.execute.restore();
+    after(() => {
+      productModel.getAll.restore();
     });
     it('Retorna um array', async () => {
-      const result = await productModel.getAll();
+      const result = await productService.getAll();
       expect(result).to.be.an('array');
     });
 
     it('O array tem que estar vazio', async () => {
-      const result = await productModel.getAll();
+      const result = await productService.getAll();
       expect(result).to.be.empty;
     });
   });
   describe('Quando existe produtos cadastrados', () => {
     before(() => {
       const executeResult = [allProductsResponse, []];
-      sinon.stub(connection, 'execute').resolves(executeResult);
+      sinon.stub(productModel, 'getAll').resolves(executeResult);
     });
     after(() => {
-      connection.execute.restore();
+      productModel.getAll.restore();
     });
 
     it('Retorna um array', async () => {
-      const result = await productModel.getAll();
+      const result = await productService.getAll();
       expect(result).to.be.an('array');
     });
     it('O array não esta vazio', async () => {
-      const result = await productModel.getAll();
+      const result = await productService.getAll();
       expect(result).to.be.not.empty;
     });
     it('O array possui itens do tipo objeto', async () => {
-      const result = await productModel.getAll();
-      expect(result[0]).to.be.an('object');
+      const result = await productService.getAll();
+      const product = result[0][0];
+      expect(product).to.be.an('object');
     });
     it('O objeto tem as propriedades "id", "name"', async () => {
-      const result = await productModel.getAll();
-      const product = result[0];
-      expect(product).to.include.all.keys('id', 'name');
+      const result = await productService.getAll();
+      const product = result[0][0];
+      expect(product).to.have.all.keys('id', 'name');
     });
   });
 });
