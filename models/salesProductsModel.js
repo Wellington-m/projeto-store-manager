a01da8d31e12221aa4e63fac006e9373a664fbf5
Wellington <wellingtonmachioni@gmail.com>
2022-08-21 18:37:52 -0300

    Implementado função para cadastrar as informações de uma venda na tabela sales_products

--- models/salesProductsModel.js
+++ models/salesProductsModel.js
@@ -4,3 +4,13 @@ const getAll = async () => {
   const [rows] = connection.execute('SELECT * FROM StoreManager.sales_products;');
   return rows;
 };
+
+const create = async (saleId, productId, quantity) => { 
+  const [rows] = connection.execute(
+    'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
+    [saleId, productId, quantity],
+  );
+  return rows;
+};
+
+module.exports = { getAll, create };
