58ec3b5d11ed602005752443a63fd4c8a6d61b78
Wellington <wellingtonmachioni@gmail.com>
2023-07-25 16:57:14 -0300

    refactor: ajustado formatação no auto-save

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,7 +19,6 @@ const getById = async (req, res) => {
     if (!result) return res.status(404).json({ message: 'Product not found' });
     return res.status(200).json(result);
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ message: ERROR_MESSAGE });
   }
 };
@@ -30,12 +29,11 @@ const create = async (req, res) => {
     const result = await productService.create(name);
     return res.status(201).json({ id: result.insertId, name });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ message: ERROR_MESSAGE });
   }
 };
 
-const update = async (req, res) => { 
+const update = async (req, res) => {
   const { name } = req.body;
   const { id } = req.params;
   const result = await productService.update(name, id);
