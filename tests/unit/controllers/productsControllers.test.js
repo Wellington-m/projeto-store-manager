14270b0fac8e0e561d9fb6711c3c284a55e1244c
Wellington <wellingtonmachioni@gmail.com>
2023-07-26 16:27:23 -0300

    test: implementado teste da função create na camada controller

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -141,11 +141,54 @@ describe('Buscar um produto pelo ID', () => {
 
 describe('Cadastrar um produto', () => {
   describe('Produto é cadastrado com sucesso', () => {
-    it('Deve retornar o status 201', () => {});
-    it('Deve retornar o id do produto e o nome', () => {});
+    const request = { body: { name: 'teste' } };
+    const response = {};
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      sinon
+        .stub(productService, 'create')
+        .resolves({ insertId: 1, name: 'teste' });
+    });
+
+    after(() => {
+      productService.create.restore();
+    });
+
+    it('Deve retornar o status 201', async () => {
+      await productController.create(request, response);
+      expect(response.status.calledWith(201)).to.be.equal(true);
+    });
+    it('Deve retornar o id do produto e o nome', async () => {
+      await productController.create(request, response);
+      expect(response.json.args[0]).to.deep.include({ id: 1, name: 'teste' });
+    });
   });
   describe('Caso ocorra um erro na aplicação', () => {
-    it('Deve retornar status 500', () => {});
-    it('Deve retornar a mensagem SERVER ERROR', () => {});
+    const request = { body: { name: 'teste' } };
+    const response = {};
+
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(productService, 'create').throws(new Error('Server error'));
+    });
+
+    after(() => {
+      productService.create.restore();
+    });
+
+    it('Deve retornar status 500', async () => {
+      await productController.create(request, response);
+      expect(response.status.calledWith(500)).to.be.equal(true);
+    });
+    it('Deve retornar a mensagem SERVER ERROR', async () => {
+      await productController.create(request, response);
+      expect(response.json.args[0]).to.deep.include({
+        message: 'Server error',
+      });
+    });
   });
 });
